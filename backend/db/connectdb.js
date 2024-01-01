cf36c6fea651f12d9e79f283c356fea35190a90e
Faizan Ali <151016446+faizan1236@users.noreply.github.com>
2023-12-31 21:56:24 -0800

    new changing

--- backend/db/connectdb.js
+++ backend/db/connectdb.js
@@ -1,12 +1,12 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const connectDB = async (DATABASE_URL) => {
-  try {
-    await mongoose.connect(DATABASE_URL);
-    console.log("Database connected successfully...");
-  } catch (error) {
-    throw new Error("Error connecting to the database:", error);
-  }
-};
+// const connectDB = async (DATABASE_URL) => {
+//   try {
+//     await mongoose.connect(DATABASE_URL);
+//     console.log("Database connected successfully...");
+//   } catch (error) {
+//     throw new Error("Error connecting to the database:", error);
+//   }
+// };
 
-module.exports = connectDB;
+// module.exports = connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const connectDB = require("./db/connectdb");
+const mongoose = require('mongoose');
 const dotenv = require("dotenv");
 const cors = require("cors");
 
@@ -13,7 +13,13 @@ app.use("/todo", todo);
 const port = 4000;
 
 const DATABASE_URL = process.env.DATABASE_URL;
-connectDB(DATABASE_URL);
+// connectDB(DATABASE_URL);
+try {
+   mongoose.connect(DATABASE_URL);
+  console.log("Database connected successfully...");
+} catch (error) {
+  throw new Error("Error connecting to the database:", error);
+}
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
